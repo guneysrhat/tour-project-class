ba87e798fafc9eecc166a292d5f8ef1314fd8091
guneysrhat <guneysrhat@gmail.com>
2022-10-11 20:52:00 +0300

    header

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import "./App.scss";
+import Header from "./components/Header/Header";
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   return (
     <div className="App">
-      <h1>Welcome to React</h1>
-      <p>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Expedita,
-        reiciendis adipisci. Dolor laudantium repellat nobis aspernatur! Vel
-        rerum illum ratione consequuntur magni, nisi illo odio! Dolorem
-        necessitatibus provident corporis nisi!
-      </p>
+      <Navbar />
+      <Header />
     </div>
   );
 }
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,7 +3,12 @@ import "./Navbar.scss";
 const Navbar = () => {
   return (
     <div className="navbar">
-      <h1>Navbar</h1>
+      <a href="#">About Us</a>
+      <a href="#">For You</a>
+      <a href="#">Services</a>
+      <a href="#">Blog</a>
+      <a href="#">Vlog</a>
+      <a href="#">Content</a>
     </div>
   );
 };
