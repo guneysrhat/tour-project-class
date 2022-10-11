faeb2c0841df6540364bded982ed63df48b8499a
guneysrhat <guneysrhat@gmail.com>
2022-10-11 20:37:22 +0300

    navbar

--- src/App.js
+++ src/App.js
@@ -4,6 +4,12 @@ function App() {
   return (
     <div className="App">
       <h1>Welcome to React</h1>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Expedita,
+        reiciendis adipisci. Dolor laudantium repellat nobis aspernatur! Vel
+        rerum illum ratione consequuntur magni, nisi illo odio! Dolorem
+        necessitatibus provident corporis nisi!
+      </p>
     </div>
   );
 }
