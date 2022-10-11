d6a8b71e20bd4058ffb01e9abc6ac6754d9fbafa
guneysrhat <guneysrhat@gmail.com>
2022-10-11 20:37:44 +0300

    nevbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,11 @@
-import './Navbar.scss'
+import "./Navbar.scss";
 
 const Navbar = () => {
-    return (
-        <div className="navbar">
-            
-        </div>
-    )
-}
+  return (
+    <div className="navbar">
+      <h1>Navbar</h1>
+    </div>
+  );
+};
+
+export default Navbar;
